65c060ace144a9044f79c4cdd543a6a6a567c81a
Divine Edwin <edwindivine5@gmail.com>
2022-07-28 13:51:24 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+'use strict'
+//set initial value
+let count = 0;
+
+//selecting values and buttons
+const value = document.querySelector('.number');
+const btns = document.querySelectorAll ('.btn');
+
+//Making the buttons functional
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains("decrease")) {
+            count--;
+        } else if (styles.contains("increase")) {
+            count++;
+        } else {
+            count = 0;
+        }
+        value.textContent = count;
+
+        //Change of value color
+        if (count > 0) {
+            value.style.color = 'green';
+        } else if (count < 0) {
+            value.style.color = 'red';
+        } else {
+            value.style.color = 'black'
+        }
+    });
+});
