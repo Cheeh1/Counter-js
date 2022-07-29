1e1a371dac186b3cba33a22e8f4208de16b2eea8
Divine Edwin <edwindivine5@gmail.com>
2022-07-29 11:16:17 -0700

    scnd commit
    alternate option

--- script.js
+++ script.js
@@ -29,3 +29,28 @@ btns.forEach(function (btn) {
         }
     });
 });
+
+
+//Using for loop
+// for (let i = 0; i < btns.length; i++)
+//   btns[i].addEventListener('click', function (e) {
+//     const styles = e.currentTarget.classList;
+//             if (styles.contains("decrease")) {
+//                 count--;
+//             } else if (styles.contains("increase")) {
+//                 count++;
+//             } else {
+//                 count = 0;
+//             }
+//             value.textContent = count;
+
+            
+//         //Change of value color
+//         if (count > 0) {
+//             value.style.color = 'green';
+//         } else if (count < 0) {
+//             value.style.color = 'red';
+//         } else {
+//             value.style.color = 'black'
+//         }
+//   });
